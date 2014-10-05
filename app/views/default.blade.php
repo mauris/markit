767fad8572597dc065742f336c391f0cb3d91d90
Sam-Mauris Yong <mauris@hotmail.sg>
2014-10-05 23:25:14 +0800

    updated default theme css

--- app/views/default.blade.php
+++ app/views/default.blade.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title></title>
+        <title>{{ isset($title) ? $title : '' }}</title>
         <base href="{{URL::to('/')}}">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-        <link href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-        <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/js/bootstrap.min.js"></script>
         <link href="theme/default/default.css" rel="stylesheet" type="text/css">
+        <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.2.0/js/bootstrap.min.js"></script>
     </head>
     <body>
-        {{$content}}
+        <div class="container">
+            {{ isset($content) ? $content : '' }}
+            @yield('content')
+        </div>
     </body>
 </html>
