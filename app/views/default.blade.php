566caef19b6b773eb5ac624996d6fd615827038f
Sam-Mauris Yong <mauris@hotmail.sg>
2014-10-15 06:47:24 +0800

    removed base href tag for better navigation support

--- app/views/default.blade.php
+++ app/views/default.blade.php
@@ -2,10 +2,9 @@
 <html lang="en">
     <head>
         <title>{{ isset($title) ? $title : '' }}</title>
-        <base href="{{URL::to('/')}}/">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-        <link href="theme/default/default.css" rel="stylesheet" type="text/css">
+        <link href="{{URL::to('/')}}/theme/default/default.css" rel="stylesheet" type="text/css">
         <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.2.0/js/bootstrap.min.js"></script>
     </head>
     <body>
