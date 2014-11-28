d58580b73dbae85f0eb02229ab3f64c50b4a8c67
Sam-Mauris Yong <mauris@hotmail.sg>
2014-11-28 08:57:03 +0800

    removed ternary operator

--- app/views/default.blade.php
+++ app/views/default.blade.php
@@ -11,7 +11,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    {{ isset($content) ? $content : '' }}
+                    {{ $content or '' }}
                     @yield('content')
                 </div>
             </div>
